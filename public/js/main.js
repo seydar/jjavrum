1b5e6bca1b394412a01b595fe4a127563caf8bb3
Jordan VanderZwaag <jvanderz@umich.edu>
2014-04-06 23:41:51 -0400

    Back-end is fully functional (hopefully) for reading, writing, and deleting from the database. Included some example use case stuff in the front end for reference

--- public/js/main.js
+++ public/js/main.js
@@ -157,23 +157,14 @@ function DataObject(data){
 	
 }//End of Map Code
 
+//code for submitting a post request to the server
 $(document).ready(function() { 
     var options = { 
-        //target:        '#output2',   // target element(s) to be updated with server response 
         beforeSubmit:  showRequest,  // pre-submit callback 
         success:       showResponse,  // post-submit callback 
         url: "api/addObject?",
  		resetForm: true
 
-        // other available options: 
-        //url:       url         // override for form's 'action' attribute 
-        //type:      type        // 'get' or 'post', override for form's 'method' attribute 
-        //dataType:  null        // 'xml', 'script', or 'json' (expected server response type) 
-        //clearForm: true        // clear all form fields after successful submit 
-        //resetForm: true        // reset the form after successful submit 
- 
-        // $.ajax options can be used here too, for example: 
-        //timeout:   3000 
     }; 
  
     // bind to the form's submit event 
@@ -184,8 +175,6 @@ $(document).ready(function() {
     	console.log($("#postData").formSerialize())
 	    console.log(options)
 
-        // inside event callbacks 'this' is the DOM element so we first 
-        // wrap it in a jQuery object and then invoke ajaxSubmit 
         $(this).ajaxSubmit(options); 
  
         // !!! Important !!! 
@@ -194,56 +183,22 @@ $(document).ready(function() {
     }); 
 }); 
  
-// pre-submit callback 
+//callback called just before uploading to server
 function showRequest(formData, jqForm, options) { 
     // formData is an array; here we use $.param to convert it to a string to display it 
     // but the form plugin does this for you automatically when it submits the data 
-    var queryString = $.param(formData); 
- 
-    // jqForm is a jQuery object encapsulating the form element.  To access the 
-    // DOM element for the form do this: 
-    // var formElement = jqForm[0]; 
- 
+    var queryString = $.param(formData);  
     alert('About to submit: \n\n' + queryString); 
- 
-    // here we could return false to prevent the form from being submitted; 
-    // returning anything other than false will allow the form submit to continue 
-    return true; 
+    return true;
 } 
  
-// post-submit callback 
+// callback called after successful upload to the server
 function showResponse(responseText, statusText, xhr, $form)  { 
-    // for normal html responses, the first argument to the success callback 
-    // is the XMLHttpRequest object's responseText property 
- 
-    // if the ajaxSubmit method was passed an Options Object with the dataType 
-    // property set to 'xml' then the first argument to the success callback 
-    // is the XMLHttpRequest object's responseXML property 
- 
-    // if the ajaxSubmit method was passed an Options Object with the dataType 
-    // property set to 'json' then the first argument to the success callback 
-    // is the json data object returned by the server 
  
     alert('status: ' + statusText + '\n\nresponseText: \n' + responseText + 
         '\n\nThe output div should have already been updated with the responseText.'); 
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+/*
 function previewImage(input) {
 	console.log("preview")
 	var preview = document.getElementById('picture');
@@ -261,25 +216,29 @@ function previewImage(input) {
 	} else {
 	  preview.setAttribute('src', '/images/imposter.png');
 	}
-}
+}*/
 
-//Determines which button it should be
+//opens dialog for choosing a file to submit
+//change to support opening of the camera later?
+//don't quite remember how this works in terms of a device's camera
 function chatOrPic(input){
 	console.log("chat or pic")
 	var $chatPicButt = $('#chatPicButt');
-	var preview = document.getElementById('picture');
+	//var preview = document.getElementById('picture');
 	if ($chatPicButt.attr("value") == "picture"){
 		document.getElementById('uploadedImage').click();
 	}else{
 		iconPicSize = getIconPicSize(preview.clientWidth,preview.clientHeight);
 		$.mobile.changePage('#map', { transition: "slide"} );
 	}
-}
+}//End of all post request code 
 
-//submits give item form
-//function formSubmit() {
 
-/*
+
+//sample get request to retrieve data from the server
+//should be changed to support the user's actual location
+function getRequest() {
+
 	//sample GET request
 	var latitude = -30.343;
 	var longitude = 23.345;
@@ -292,17 +251,23 @@ function chatOrPic(input){
 			alert("Error")
 		},
 		success: function(result){
+			//this is an example of how the returned data can be used to display an image
+			//see img tag in index.html
+			var $picture = $('#picture');
 			alert("Successful get!")
 			console.log(result);
+			//console.log(result[0]["image"])
+			$picture.attr("src", "data:image/jpg;base64," + result[0]["image"])
 		}
 	})
-	
-*/
+}
+
 
 /*
 	//sample DELETE request
-	//set deleteId equal to object's _id field
-	var deleteId = 533ae6ba4fad157c32000003;
+	//set deleteId equal to object's _id field*/
+function deleteRequest() {
+	var deleteId = "53421b267608b78025000001";
 	$.ajax({
 		url: "api/deleteObject/" +  deleteId,
 		type:"DELETE",
@@ -314,37 +279,4 @@ function chatOrPic(input){
             
         }
 	})
-*/
-
-
-	//sample POST request
-	//actual data should be gotten from user input and location should be gotten through Maps API
-/*
-	var longitude = 30.232;
-	var latitude = -23.432;
-	var name = "My couch";
-	var description = "This is my old couch";
-	var category = "Furniture";
-	var pickUp = "Pick up in my front yard";
-	var available = 7;
-	var image = "..."
-
-	dataString = "&longitude=" + longitude + "&latitude=" + latitude + "&name=" + name;
-	dataString+= "&description=" + description + "&category=" + category + "&pickUp=" + pickUp;
-	dataString += "&available=" + available + "&image=" + image;
-
-	$.ajax({
-		url: "api/addObject?" + dataString,
-		type:'POST',
-		success: function(result) {
-			alert("Successful post!")
-		},
-		error: function(xhr, status) {
-			console.log(status);
-		},
-		complete: function(result){
-			console.log(result);
-		}
-	})
-	
-}*/
+}
--- server/read.js
+++ server/read.js
@@ -1,7 +1,9 @@
 var url = require("url");
 var mongo = require('mongodb');
-var GridFS = require('GridFS').GridFS;
-var GridStream = require('GridFS').GridStream
+var fs = require('fs');
+var ObjectID = require("mongodb").ObjectID
+var Db = mongo.Db;
+var Grid = mongo.Grid;
 
 
 // Database Code
@@ -9,6 +11,8 @@ var monk = require('monk');
 var db = monk('localhost:27017/justakeDB');
 var collection = db.get('data');
 
+var imagesDB = new mongo.Db('imageDB', new mongo.Server("127.0.0.1",27017), {safe: false});
+var gfs = Grid(imagesDB, mongo);
 
 //converts miles to meters
 function milesToMeters(miles){
@@ -39,28 +43,112 @@ function queryBuilder(queryObject){
 	return returnQuery;
 }
 
+/*retrieves a file from the imagesDB based on 
+on the fileID based to it and calls a callback with
+the file as a binary*/
+function retrieveImage(fileId, callback) {
+	console.log("retrieveImage")
+	Db.connect("mongodb://localhost:27017/imagesDB", function(err, imagesDB) {
+
+		var grid = new Grid(imagesDB, 'fs');
+
+		grid.get(fileId, function(err, result) {
+			console.log(result)
+			//docs[i].image = result
+			callback(result)
+		})
+	});
+}
+
+/*for each document passed into the docs array, 
+retrieveImage is called to get the associated file from
+the document's fileID value. Once this has been successfully 
+executed on all docs in the array, a callback is returned 
+with the every document object also including the associated file as a binary
+*/function matchDocs(docs, callback){
+	console.log("matchDocs")
+	var inserted = 0;
+	resultsArray = [];
+	for (var i = 0; i < docs.length; i++) {
+		var doc = docs[i];
+		var fileId = docs[i].fileId;
+		console.log(fileId)
+		retrieveImage(fileId, function(result) {
+			console.log("callback called")
+			console.log(result)
+			//convert binary data to base64 encoded string
+			doc.image = result.toString('base64')
+			doc.latitude = doc.loc.coordinates[1];
+			doc.longitude = doc.loc.coordinates[0];
+			resultsArray.push(doc)
+
+			if (resultsArray.length == docs.length) {
+				console.log("sending callback")
+				equal = true;
+				callback(resultsArray)
+			}
+			else{
+				console.log("not yet")
+				console.log(resultsArray.length)
+				console.log(docs.length)
+			}
+			//docs[i].image = result
+		})
+		
+	}
+	/*var equal = false;
+	//checks to see if all of the callbacks have successfully completed
+	setTimeout(function() {
+		while(!equal) {
+			console.log("running")
+			console.log(resultsArray);
+			
+		}
+
+	}, 3)*/
+}
+
 /*Parses the query string given by the URL, finds objects that pass the filter in the database
 and returns them*/
 exports.getObjects = function(req, res) {
-	var searchQuery = queryBuilder(req.query);
-	var latitude;
-	var longitude;
-	if (parseFloat(req.query.longitude) && parseFloat(req.query.latitude)){
-		longitude = parseFloat(req.query.longitude)
-		latitude = parseFloat(req.query.latitude);
-		console.log("longitude = " + longitude);
-		console.log("latitude = " + latitude);
-		var orderBy = {"$geometry" : {"type": "Point", "coordinates": [longitude, latitude]}}
-		collection.find( { $query: searchQuery,  $orderby: {"$near": [orderBy]}} , function(err, docs) {
-			//console.log(docs)
-			console.log("success")
+	console.log("get request")
+	Db.connect("mongodb://localhost:27017/imagesDB", function(err, imagesDB) {
+		var grid = new Grid(imagesDB, 'fs');
+		var searchQuery = queryBuilder(req.query);
+		var latitude;
+		var longitude;
+		if (parseFloat(req.query.longitude) && parseFloat(req.query.latitude)){
+			longitude = parseFloat(req.query.longitude)
+			latitude = parseFloat(req.query.latitude);
+			console.log("longitude = " + longitude);
+			console.log("latitude = " + latitude);
+			var orderBy = {"$geometry" : {"type": "Point", "coordinates": [longitude, latitude]}}
+			collection.find( { $query: searchQuery,  $orderby: {"$near": [orderBy]}} , function(err, docs) {
+				//console.log(docs)
+				//console.log("success")
+				//console.log(docs)
+				//if nothing found, return immediately
+				if (docs.length == 0) {
+					res.send(200)
+				}
+				var inserted = 0;
+				var resultArray = [];
+				matchDocs(docs, function(resultsArray) {
+					console.log("matchDocs callback")
+					console.log(resultsArray)
+					for (i = 0; i < resultsArray.length; i++) {
 
-			res.send(docs)
-		
-		});
-	}
-	else {
-		console.log("error")
-		res.error("Longitude and Longitude required");
-	}
+					}
+					res.send(resultsArray)
+				})
+				
+			});
+		}
+		else {
+			console.log("error")
+			res.error("Longitude and Longitude required");
+		}
+
+	})
+	
 }
