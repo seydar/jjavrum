9fb6de4b091ebb642488f0216be3c6979759cde6
Jordan VanderZwaag <jvanderz@umich.edu>
2014-04-15 18:49:17 -0400

    set sentence structure handling to be oop and included proper print functions

--- sentencestructure.py
+++ sentencestructure.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import nltk.data
 import sys
 import nltk
@@ -7,59 +8,101 @@ from nltk.tag import pos_tag
 from collections import Counter
 
 
-fileName = sys.argv[-1]
-fileObj = open(fileName, 'r')
-fileString = fileObj.read()
 
+
+
+class StructureParser:
+	def __init__(self, filePath):
+		self.fileObj = open(fileName, 'r')
+		self.fileString = self.fileObj.read()
+		self.fileStringCounter = Counter(self.fileString)
+		self.sentences = tokenize.sent_tokenize(self.fileString)
+		self.numSentences = len(self.sentences)
+		self.tokens = self.defineTokens()
+		self.sentenceLength = self.findSentenceLength()
+		self.commaRate = self.findCommaRate()
+		self.semicolonRate = self.findSemicolonRate()
+		self.colonRate = self.findColonRate()
+		self.quotationRate = self.findQuotationRate()
+		self.conjunctionRate = self.findConjunctionRate()
+		self.wordLength = self.findWordLength()
+	
+	def defineTokens(self):
+		tokens = []
+		for sentence in self.sentences:
+			sentenceTokens = nltk.word_tokenize(sentence)
+			tokens.extend(sentenceTokens)
+		return tokens
+
+	def findSentenceLength(self):
+		totalLength = 0
+		for sentence in self.sentences:
+			sentenceCounter = Counter(sentence)
+			length = sentenceCounter[" "]
+			length += 1
+			totalLength += length
+		avgSentenceLength = float(totalLength)/self.numSentences
+		return avgSentenceLength
+	
+	def findCommaRate(self):
+		commas = self.fileStringCounter[',']
+		return float(commas)/self.numSentences
+
+	def findSemicolonRate(self):
+		semicolons = self.fileStringCounter[";"]
+		return float(semicolons)/self.numSentences
+
+	def findColonRate(self):
+		colons = self.fileStringCounter[":"]
+		return float(colons)/self.numSentences
+
+	def findQuotationRate(self):
+		quotations = self.fileStringCounter['"']
+		return float(quotations)/self.numSentences
+
+	def findConjunctionRate(self):
+		taggedTokens = nltk.pos_tag(self.tokens)
+		conjunctionsList = [item for item in taggedTokens if "CC" in item]
+		numConjunctions = len(conjunctionsList)
+		return float(numConjunctions)/self.numSentences
+
+	def findWordLength(self):
+		totalWordLength = 0
+		totalWords = 0
+		for word in self.tokens:
+			if len(word) == 1:
+				if word.lower() == "a" or word.lower() == "i":
+					totalWords += 1
+					totalWordLength += 1
+			else:
+				totalWords += 1
+				totalWordLength += len(word)
+		return float(totalWordLength)/totalWords
+
+	#don't print out new line character at end
+	def printPretty(self):
+		print("average sentence length: " + str(self.sentenceLength))
+		print("commas per sentence: " + str(self.commaRate))
+		print("semicolons per sentence: " + str(self.semicolonRate))
+		print("colons per sentence: " + str(self.colonRate))
+		print("quotation marks per sentence: " + str(self.quotationRate))
+		print("conjunctations per sentence: " + str(self.conjunctionRate))
+		print("average word length: " + str(self.wordLength))
+
+	def printComputer(self):
+		print(str(self.sentenceLength))
+		print(str(self.commaRate))
+		print(str(self.semicolonRate))
+		print(str(self.colonRate))
+		print(str(self.quotationRate))
+		print(str(self.conjunctionRate))
+		print(str(self.wordLength), end = "")
+
+if __name__ == "__main__":
+	fileName = sys.argv[-1]
+	structureParser = StructureParser(fileName)
+	structureParser.printPretty()
+	structureParser.printComputer()
 
 #install punkt package to get this to work.
 #Uncomment nltk.download()
-sentences =  tokenize.sent_tokenize(fileString)
-
-counter = Counter(fileString)
-commas = counter[',']
-semicolons = counter[';']
-colons = counter[':']
-quotations = counter['"']
-
-
-commaRate = float(commas)/len(sentences)
-semicolonRate = float(semicolons)/len(sentences)
-colonRate = float(colons)/len(sentences)
-quotationRate = float(quotations)/len(sentences)
-
-print "commas per sentence: " + str(commaRate)
-print "semicolons per sentence: " + str(semicolonRate)
-print "colons per sentence: " + str(colonRate)
-print "quotation marks per sentence: " + str(quotationRate)
-
-totalLength = 0
-for sentence in sentences:
-	sentenceCounter = Counter(sentence)
-	length = sentenceCounter[" "]
-	length += 1
-	totalLength += length
-avgSentenceLength = float(totalLength)/len(sentences)
-print "average sentence length: " + str(avgSentenceLength)
-	
-conjunctions = 0
-words = 0
-totalWordLength = 0	
-for sentence in sentences: 
-	tokens = nltk.word_tokenize(sentence)
-	taggedTokens = nltk.pos_tag(tokens)
-	conjunctionsList = [item for item in taggedTokens if "CC" in item]
-	conjunctions += len(conjunctionsList)
-	for word in tokens:
-		if len(word) == 1:
-			if word.lower() == "a" or word.lower() == "i":
-				words += 1
-				totalWordLength += 1
-		else:
-			words += 1
-			totalWordLength += len(word)
-
-conjunctionRate = float(conjunctions)/len(sentences)
-avgWordLength = float(totalWordLength)/words
-print "conjunctations per sentence: " + str(conjunctionRate)
-print "average word length: " + str(avgWordLength)
