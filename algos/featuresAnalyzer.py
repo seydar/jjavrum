617e87625f6ee78462b930c0e79d6c30cc99c111
Jordan VanderZwaag <jvanderz@umich.edu>
2014-04-17 00:40:59 -0400

    added sentence structure analysis to find average subject and verb position and find the average max depth of each sentence

--- algos/featuresAnalyzer.py
+++ algos/featuresAnalyzer.py
@@ -4,17 +4,20 @@ import sys
 import nltk
 import os
 import operator
+import re
 from nltk import tokenize
 from nltk import word_tokenize
 #from nltk.parse  import stanford
 from nltk.tag import pos_tag
 from collections import Counter
+import codecs
 #os.environ['STANFORD_PARSER'] = 'C:/stanford-parser-2012-11-12'
 #os.environ['STANFORD_MODELS'] = 'C:/stanford-parser-2012-11-12'
 #parser = stanford.StanfordParser(model_path="C:/stanford-parser-2012-11-12/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 
-
-
+#parser = StanfordParser("C:/stanford-parser-2012-11-12/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+#sentence = parser.parse("This is a test.")
+#sentence.print_tree()
 
 
 class FeaturesAnalyzer:
@@ -123,8 +126,98 @@ class FeaturesAnalyzer:
 				trigramsDict[encodedTrigram] = 1
 		sortedTrigrams = sorted(trigramsDict.iteritems(), key=operator.itemgetter(1), reverse=True)
 		return sortedTrigrams[:10]
+	
+	def setAvgPositions(self):
+		totalSubjPercent = 0
+		totalVerbPercent = 0
+		totalLength = 0
+		totalDepth = 0
 
+		for sentence in self.sentences:
+			command = 'java -classpath stanford-parser.jar;stanford-parser-3.3.1-models.jar -mx200m edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTMPSubcategories -outputFormat "wordsAndTags,penn,typedDependencies" stanford-parser-3.3.1-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz'
+			currentDir = os.getcwd()
+			os.chdir("/stanford-parser-full-2014-01-04")
+			sentenceEncoded = sentence.encode('utf-8')
+			os.popen("echo "+sentenceEncoded+" > /Temp/stanford.txt")
 
+			sentenceLength = len(sentence)
+
+			parser_out = os.popen(command + " /Temp/stanford.txt").readlines()
+			os.chdir(currentDir)
+			foundSubj = False
+			lastSubj = ""
+			lastVerb = ""
+			currentDepth = 0
+			maxDepth = 0
+			for i, tag in enumerate(parser_out):
+				if len(tag.strip()) > 0 and tag.strip()[0] == '(':
+					singleClosingChar = re.compile('[^\)]\)')
+					openParanth = tag.count('(')
+					closedParanth = tag.count(')')
+					singleCloseParanth = len(singleClosingChar.findall(tag))
+					closingDif = closedParanth - singleCloseParanth
+					addNumber = 0
+
+					if (singleCloseParanth > 0):
+						addNumber = 1
+					potentialMaxDepth = currentDepth + openParanth - singleCloseParanth + addNumber
+					#print("potential max depth: " + str(potentialMaxDepth))
+					#print("currentDepth " + str(currentDepth))
+					#print("currentMaxDepth " + str(maxDepth))
+					if potentialMaxDepth > maxDepth:
+						maxDepth = potentialMaxDepth
+					currentDepth = currentDepth + openParanth - closedParanth
+					#print("new currentDepth " + str(currentDepth))
+					#print("tag" + tag)
+					#print("open paranth " + str(openParanth))
+					#print("closed paranth " + str(closedParanth))
+					#print("single close paranth " + str(singleCloseParanth))
+				elif len(tag.strip()) > 0:
+					#print(i, "Typed dependencies: ", tag) 
+					if tag.find("nsub") >= 0:
+						if foundSubj == True:
+							print("multiple subjects found")
+						foundSubj = True
+						#print(sentenceEncoded)
+
+						#print(tag)
+						
+						verbStartPos = tag.find("(")+1
+						spacePos = tag.rfind(" ")
+						subj = tag[spacePos+1:-2]
+						verb = tag[verbStartPos:spacePos-1]
+						subjDashPos = subj.rfind("-")
+						verbDashPos = verb.rfind("-")
+
+						#print(verbDashPos)
+						#print(tag)
+						#print(subj)
+						#print("verb: " + verb)
+						subjPos = subj[subjDashPos+1:]
+						verbPos = verb[verbDashPos+1:]
+						#print(verbPos)
+						#print(subjPos)
+						if lastSubj != subj:
+							totalSubjPercent += float(subjPos)/sentenceLength
+						else:
+							print("same subject found")
+						if lastVerb != verb:
+							totalVerbPercent += float(verbPos)/sentenceLength
+						lastSubj = subj
+						lastVerb = verb
+			print(maxDepth)
+			totalDepth += maxDepth
+			if foundSubj == False:
+				print(sentenceEncoded)
+		avgSubjPos = totalSubjPercent/self.numSentences
+		avgVerbPos = totalVerbPercent/self.numSentences
+		avgMaxDepth = float(totalDepth)/self.numSentences
+		self.avgSubjPos = avgSubjPos
+		self.avgVerbPos = avgVerbPos
+		self.avgMaxDepth = avgMaxDepth
+		print(avgSubjPos)
+		print(avgVerbPos)
+		print(avgMaxDepth)
 	#don't print out new line character at end
 	def printPretty(self):
 		print("average sentence length: " + str(self.sentenceLength))
@@ -162,8 +255,35 @@ if __name__ == "__main__":
 
 	
 	featuresAnalyzer = FeaturesAnalyzer(fileName)
-	featuresAnalyzer.printPretty()
-	featuresAnalyzer.printComputer()
+	featuresAnalyzer.setAvgPositions()
+	#featuresAnalyzer.printPretty()
+	#featuresAnalyzer.printComputer()
+	#download stanford-parser-3.3.1 and unzip into C directory
+	#unzip stanford-parser-3.3.1-models.jar in that directory
+	#command = 'java -classpath stanford-parser.jar;stanford-parser-3.3.1-models.jar -mx200m edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTMPSubcategories -outputFormat "wordsAndTags,penn,typedDependencies" stanford-parser-3.3.1-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz'
+	#sentence =  "I'm leaving now!"
+	#currentDir = os.getcwd()
+	#os.chdir("/stanford-parser-full-2014-01-04")
+
+	#os.popen("echo "+sentence+" > /Temp/stanford.txt")
+
+	#parser_out = os.popen(command + " /Temp/stanford.txt").readlines()
+	#os.chdir(currentDir)
+	#print(parser_out)
+	#for i, tag in enumerate(parser_out):
+	#	if len(tag.strip()) > 0 and tag.strip()[0] == '(':
+	#		parse = " ".join(tag.strip())
+	#		print(i, "Parse: ", tag)
+	#	elif len(tag.strip()) > 0:
+	#		print(i, "Typed dependencies: ", tag) 
+	#		if tag.find("nsub") >= 0:
+	#			print(tag)
+	#			dashPos = tag.rfind("-")
+	#			subjPos = tag[dashPos+1:-2]
+	#s			(float(subjPos))
+	#bracketed_parse = " ".join( [tag.strip() for tag in parser_out if len(tag.strip()) > 0 and tag.strip()[0] == "("] )
+	#print(bracketed_parse)
 
 #install punkt package to get this to work.
 #Uncomment nltk.download()
+#"java -classpath stanford-parser.jar;stanford-parser-3.3.1-models.jar -mx200m edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTMPSubcategories -outputFormat "wordsAndTags,penn,typedDependencies" stanford-parser-3.3.1-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz LICENSE.txt"
